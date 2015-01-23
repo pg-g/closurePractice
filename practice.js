3c379278a0ac7d51e833dae10bb72c93f4292030
Paige <paigegu3+git@gmail.com>
2015-01-22 19:04:07 -0700

    working

--- practice.js
+++ practice.js
@@ -6,21 +6,19 @@ var outer = function(){
     return 'The original name was ' + name;
   }
 }
-//Above you're given a function that returns another function which has a closure over the name variable.
+//Above you're given a function that returns another function which has a 
+//closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
-
   //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
-
   //Code Here
+inner();
 
 
 
 //Next problem
-
-
-
 var callFriend = function(){
   var friend = 'Jake';
   function callF(number){
@@ -30,21 +28,23 @@ var callFriend = function(){
 };
 
 //Above you're given a callFriend function that returns another function.
-//Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
-
+//Do what you need to do in order to call your function 
+//and get 'Calling Jake at 435-215-9248' in your console.
   //Code Here
-
+var doIt = callFriend();
+doIt('435-215-9248');
 
 
 //Next Problem
-
-
-
-/*
-  Write a function called makeCounter that makes the following code work properly.
-*/
-
+//Write a function called makeCounter that makes the following code work properly.
   //Code Here
+var makeCounter = function() {
+  var num = 1;
+  return function() {
+    return num++;
+  }
+}
+
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -54,16 +54,22 @@ var callFriend = function(){
 
 
 //Next Problem
-
-
-
 /*
-  Write a function that accepts another function as it's only argument and returns a new function
-  (which invokes the original function that was passed in) that can only ever be executed once.
-  Once completed, add a second arguments that allows the function to be invoked N number of times.
+  Write a function that accepts another function as its only argument 
+  and returns a new function 
+  (which invokes the original function that was passed in) 
+  that can only ever be executed once.
+
+  Once completed, add a second argument 
+  that allows the function to be invoked N number of times.
+  
   After the function has been called N number of times, console.log('STAHHP');
 */
-
+var firstFn = function(secondFn) {
+  return function() {
+    secondFn();
+  }
+}
 
 
 
